52e2ab1d0248f930656cd1e6ba8b8d1e477b8e16
Requred <139059035+Requred@users.noreply.github.com>
2023-07-12 23:39:12 +0500

    Update track.js

--- track.js
+++ track.js
@@ -1,41 +1,47 @@
 (function () {
     'use strict';
 
-    var connect_host = 'http://185.204.0.61';
+    var connect_host = '{localhost}';
     var list_opened = false;
-    function log() {
-      console.log.apply(console.log, arguments);
+
+    function reguest(params, callback) {
+      {
+        if (connect_host == '{localhost}') connect_host = '185.204.0.61';
+        var socket = new WebSocket('ws://' + connect_host + ':8080/?' + params.torrent_hash + '&index=' + params.id);
+        socket.addEventListener('message', function (event) {
+          socket.close();
+          var json = {};
+
+          try {
+            json = JSON.parse(event.data);
+          } catch (e) {}
+
+          if (json.streams) callback(json);
+        });
+      }
     }
-    function reguest(data, call) {
-      var net = new Lampa.Reguest();
-      var url = connect_host + '/ffprobe?media=' + encodeURIComponent(data.url);
-      var email = Lampa.Storage.get('account_email');
-      if (email) url = Lampa.Utils.addUrlComponent(url, 'account_email=' + encodeURIComponent(email));
-      log('Tracks', 'reguest', url);
-      net.timeout(1000 * 15);
-      net["native"](url, function (result) {
-        var json = {};
-        try {
-          json = JSON.parse(result);
-        } catch (e) {}
-        call(json);
-      }, call, false, {
-        dataType: 'text'
-      });
-    }
-    function subscribe(data) {
+
+    function subscribeTracks(data) {
       var inited = false;
       var inited_parse = false;
       var webos_replace = {};
+
+      function log() {
+        console.log.apply(console.log, arguments);
+      }
+
       function getTracks() {
         var video = Lampa.PlayerVideo.video();
         return video.audioTracks || [];
       }
+
       function getSubs() {
         var video = Lampa.PlayerVideo.video();
         return video.textTracks || [];
       }
+
       log('Tracks', 'start');
+
       function setTracks() {
         if (inited_parse) {
           var new_tracks = [];
@@ -61,16 +67,18 @@
               ghost: orig ? false : true,
               selected: orig ? orig.selected == true || orig.enabled == true : false
             };
-            log('Tracks', 'tracks original', orig);
+            console.log('Tracks', 'tracks original', orig);
             Object.defineProperty(elem, "enabled", {
               set: function set(v) {
                 if (v) {
                   var aud = getTracks();
                   var trk = aud[elem.index];
+
                   for (var i = 0; i < aud.length; i++) {
                     aud[i].enabled = false;
                     aud[i].selected = false;
                   }
+
                   if (trk) {
                     trk.enabled = true;
                     trk.selected = true;
@@ -84,6 +92,7 @@
           if (parse_tracks.length) Lampa.PlayerPanel.setTracks(new_tracks);
         }
       }
+
       function setSubs() {
         if (inited_parse) {
           var new_subs = [];
@@ -108,16 +117,18 @@
               ghost: video_subs[track.index - minus] ? false : true,
               selected: orig ? orig.selected == true || orig.mode == 'showing' : false
             };
-            log('Tracks', 'subs original', orig);
+            console.log('Tracks', 'subs original', orig);
             Object.defineProperty(elem, "mode", {
               set: function set(v) {
                 if (v) {
                   var txt = getSubs();
                   var sub = txt[elem.index];
+
                   for (var i = 0; i < txt.length; i++) {
                     txt[i].mode = 'disabled';
                     txt[i].selected = false;
                   }
+
                   if (sub) {
                     sub.mode = 'showing';
                     sub.selected = true;
@@ -131,35 +142,45 @@
           if (parse_subs.length) Lampa.PlayerPanel.setSubs(new_subs);
         }
       }
+
       function listenTracks() {
         log('Tracks', 'tracks video event');
         setTracks();
         Lampa.PlayerVideo.listener.remove('tracks', listenTracks);
       }
+
       function listenSubs() {
         log('Tracks', 'subs video event');
         setSubs();
         Lampa.PlayerVideo.listener.remove('subs', listenSubs);
       }
+
       function canPlay() {
         log('Tracks', 'canplay video event');
         if (webos_replace.tracks) setWebosTracks(webos_replace.tracks);else setTracks();
         if (webos_replace.subs) setWebosSubs(webos_replace.subs);else setSubs();
         Lampa.PlayerVideo.listener.remove('canplay', canPlay);
       }
+
       function setWebosTracks(video_tracks) {
         if (inited_parse) {
           var parse_tracks = inited_parse.streams.filter(function (a) {
             return a.codec_type == 'audio';
           });
           log('Tracks', 'webos set tracks:', video_tracks.length);
+
           if (parse_tracks.length !== video_tracks.length) {
-            if (webOS.sdk_version < 5) parse_tracks = parse_tracks.filter(function (a) {
+            parse_tracks = parse_tracks.filter(function (a) {
               return a.codec_name !== 'truehd';
-            });else parse_tracks = parse_tracks.filter(function (a) {
-              return a.codec_name !== 'dts' && a.codec_name !== 'truehd';
             });
+
+            if (parse_tracks.length !== video_tracks.length) {
+              parse_tracks = parse_tracks.filter(function (a) {
+                return a.codec_name !== 'dts';
+              });
+            }
           }
+
           parse_tracks = parse_tracks.filter(function (a) {
             return a.tags;
           });
@@ -172,6 +193,7 @@
           });
         }
       }
+
       function setWebosSubs(video_subs) {
         if (inited_parse) {
           var parse_subs = inited_parse.streams.filter(function (a) {
@@ -186,6 +208,7 @@
           });
           parse_subs.forEach(function (track, a) {
             var i = a + 1;
+
             if (video_subs[i]) {
               video_subs[i].language = track.tags.language;
               video_subs[i].label = track.tags.title || track.tags.handler_name;
@@ -193,29 +216,32 @@
           });
         }
       }
+
       function listenWebosSubs(_data) {
         log('Tracks', 'webos subs event');
         webos_replace.subs = _data.subs;
         if (inited_parse) setWebosSubs(_data.subs);
       }
+
       function listenWebosTracks(_data) {
         log('Tracks', 'webos tracks event');
         webos_replace.tracks = _data.tracks;
         if (inited_parse) setWebosTracks(_data.tracks);
       }
+
       function listenStart() {
         inited = true;
         reguest(data, function (result) {
-          if (result && result.streams) {
-            inited_parse = result;
-            log('Tracks', 'parsed', inited_parse);
-            if (inited) {
-              if (webos_replace.subs) setWebosSubs(webos_replace.subs);else setSubs();
-              if (webos_replace.tracks) setWebosTracks(webos_replace.tracks);else setTracks();
-            }
+          log('Tracks', 'parsed', inited_parse);
+          inited_parse = result;
+
+          if (inited) {
+            if (webos_replace.subs) setWebosSubs(webos_replace.subs);else setSubs();
+            if (webos_replace.tracks) setWebosTracks(webos_replace.tracks);else setTracks();
           }
         });
       }
+
       function listenDestroy() {
         inited = false;
         Lampa.Player.listener.remove('destroy', listenDestroy);
@@ -226,6 +252,7 @@
         Lampa.PlayerVideo.listener.remove('canplay', canPlay);
         log('Tracks', 'end');
       }
+
       Lampa.Player.listener.follow('destroy', listenDestroy);
       Lampa.PlayerVideo.listener.follow('tracks', listenTracks);
       Lampa.PlayerVideo.listener.follow('subs', listenSubs);
@@ -234,22 +261,34 @@
       Lampa.PlayerVideo.listener.follow('canplay', canPlay);
       listenStart();
     }
+
     function parseMetainfo(data) {
       var loading = Lampa.Template.get('tracks_loading');
       data.item.after(loading);
       reguest(data.element, function (result) {
-        if (list_opened && result && result.streams) {
+        if (list_opened) {
           var append = function append(name, fields) {
             if (fields.length) {
-              var where = html.find('.tracks-metainfo__item-' + name + ' .tracks-metainfo__info');
-              fields.slice(0, 4).forEach(function (i) {
-                where.append('<div>' + i + '</div>');
+              var block = Lampa.Template.get('tracks_metainfo_block', {});
+              block.find('.tracks-metainfo__label').text(Lampa.Lang.translate(name == 'video' ? 'extensions_hpu_video' : name == 'audio' ? 'player_tracks' : 'player_' + name));
+              fields.forEach(function (data) {
+                var item = $('<div class="tracks-metainfo__item tracks-metainfo__item--' + name + ' selector"></div>');
+                item.on('hover:focus', function (e) {
+                  Lampa.Modal.scroll().update(item);
+                });
+
+                for (var i in data) {
+                  var div = $('<div class="tracks-metainfo__column--' + i + '"></div>');
+                  div.text(data[i]);
+                  item.append(div);
+                }
+
+                block.find('.tracks-metainfo__info').append(item);
               });
-              if (fields.length > 4) where.append('<div>' + Lampa.Lang.translate('more') + ' +' + (fields.length - 4) + '</div>');
-            } else {
-              html.find('.tracks-metainfo__item-' + name).remove();
+              html.append(block);
             }
           };
+
           var video = [];
           var audio = [];
           var subs = [];
@@ -263,54 +302,69 @@
             return a.codec_type == 'subtitle';
           });
           codec_video.slice(0, 1).forEach(function (v) {
-            var line = [];
-            if (v.width && v.height) line.push(v.width + 'х' + v.height);
-            if (v.codec_name) line.push(v.codec_name.toUpperCase());
-            if (Boolean(v.is_avc)) line.push('AVC');
-            if (line.length) video.push(line.join(' / '));
+            var line = {};
+            if (v.width && v.height) line.video = v.width + 'х' + v.height;
+            if (v.codec_name) line.codec = v.codec_name.toUpperCase();
+            if (Boolean(v.is_avc)) line.avc = 'AVC';
+            if (Lampa.Arrays.getKeys(line).length) video.push(line);
           });
           codec_audio.forEach(function (a, i) {
-            var line = [i + 1];
+            var line = {
+              num: i + 1
+            };
+
             if (a.tags) {
-              line.push(a.tags.language);
-              if (a.tags.title || a.tags.handler_name) line.push(a.tags.title || a.tags.handler_name);
+              line.lang = (a.tags.language || '').toUpperCase();
             }
-            if (a.codec_name) line.push(a.codec_name.toUpperCase());
-            if (a.channels) line.push(a.channels + ' ch.' + (a.channel_layout ? ' ' + a.channel_layout : ''));
-            if (line.length) audio.push(line.join(' / '));
+
+            line.name = a.tags ? a.tags.title || a.tags.handler_name : '';
+            if (a.codec_name) line.codec = a.codec_name.toUpperCase();
+            if (a.channel_layout) line.channels = a.channel_layout.replace('(side)', '').replace('stereo', '2.0');
+            if (a.bit_rate || a.tags.BPS || a.tags["BPS-eng"]) line.rate = Math.round((a.bit_rate || a.tags.BPS || a.tags["BPS-eng"]) / 1000) + ' Kbps';
+            if (Lampa.Arrays.getKeys(line).length) audio.push(line);
           });
           codec_subs.forEach(function (a, i) {
-            var line = [i + 1];
+            var line = {
+              num: i + 1
+            };
+
             if (a.tags) {
-              line.push(a.tags.language);
-              if (a.tags.title || a.tags.handler_name) line.push(a.tags.title || a.tags.handler_name);
+              line.lang = (a.tags.language || '').toUpperCase();
             }
-            if (line.length) subs.push(line.join(' / '));
+
+            line.name = a.tags ? a.tags.title || a.tags.handler_name : '';
+            if (Lampa.Arrays.getKeys(line).length) subs.push(line);
           });
           var html = Lampa.Template.get('tracks_metainfo', {});
           append('video', video);
           append('audio', audio);
           append('subs', subs);
+          loading.remove();
+
           if (video.length || audio.length || subs.length) {
             data.item.after(html);
           }
+
+          if (Lampa.Controller.enabled().name == 'modal') Lampa.Controller.toggle('modal');
         }
-        loading.remove();
       });
     }
+
     Lampa.Player.listener.follow('start', function (data) {
-      if (!(/\.m3u8/.test(data.url) || /\.mpd/.test(data.url) || /\.mp4/.test(data.url))) subscribe(data);
+      if (data.torrent_hash) subscribeTracks(data);
     });
     Lampa.Listener.follow('torrent_file', function (data) {
       if (data.type == 'list_open') list_opened = true;
       if (data.type == 'list_close') list_opened = false;
+
       if (data.type == 'render' && data.items.length == 1 && list_opened) {
         parseMetainfo(data);
       }
     });
     Lampa.Template.add('tracks_loading', "\n    <div class=\"tracks-loading\">\n        <span>#{loading}...</span>\n    </div>\n");
-    Lampa.Template.add('tracks_metainfo', "\n    <div class=\"tracks-metainfo\">\n        <div class=\"tracks-metainfo__half\">\n            <div class=\"tracks-metainfo__item-video\">\n                <div class=\"tracks-metainfo__label\">#{extensions_hpu_video}</div>\n                <div class=\"tracks-metainfo__info\"></div>\n            </div>\n            <div class=\"tracks-metainfo__item-audio\">\n                <div class=\"tracks-metainfo__label\">#{player_tracks}</div>\n                <div class=\"tracks-metainfo__info\"></div>\n            </div>\n        </div>\n        <div class=\"tracks-metainfo__half\">\n            <div class=\"tracks-metainfo__item-subs\">\n                <div class=\"tracks-metainfo__label\">#{player_subs}</div>\n                <div class=\"tracks-metainfo__info\"></div>\n            </div>\n        </div>\n    </div>\n");
-    Lampa.Template.add('tracks_css', "\n    <style>\n    .tracks-loading{margin-top:1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:start;-webkit-align-items:flex-start;-moz-box-align:start;-ms-flex-align:start;align-items:flex-start}.tracks-loading:before{content:'';display:inline-block;width:1.3em;height:1.3em;background:url('./img/loader.svg') no-repeat 50% 50%;-webkit-background-size:contain;-o-background-size:contain;background-size:contain;margin-right:.4em}.tracks-loading>span{font-size:1.1em;line-height:1.1}.tracks-metainfo{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;margin-top:1em;-webkit-border-radius:.3em;border-radius:.3em;background-color:rgba(0,0,0,0.22);-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap}.tracks-metainfo__half{width:50%}.tracks-metainfo__half>div{padding:1em}@media screen and (max-width:480px){.tracks-metainfo__half{width:100%}}.tracks-metainfo__label{opacity:.5;font-weight:600}.tracks-metainfo__info{padding-top:1em;line-height:1.2}.tracks-metainfo__info>div+div{margin-top:.5em}\n    </style>\n");
+    Lampa.Template.add('tracks_metainfo', "\n    <div class=\"tracks-metainfo\"></div>\n");
+    Lampa.Template.add('tracks_metainfo_block', "\n    <div class=\"tracks-metainfo__line\">\n        <div class=\"tracks-metainfo__label\"></div>\n        <div class=\"tracks-metainfo__info\"></div>\n    </div>\n");
+    Lampa.Template.add('tracks_css', "\n    <style>\n    .tracks-loading{margin-top:1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:start;-webkit-align-items:flex-start;-moz-box-align:start;-ms-flex-align:start;align-items:flex-start}.tracks-loading:before{content:'';display:inline-block;width:1.3em;height:1.3em;background:url('./img/loader.svg') no-repeat 50% 50%;-webkit-background-size:contain;-moz-background-size:contain;-o-background-size:contain;background-size:contain;margin-right:.4em}.tracks-loading>span{font-size:1.1em;line-height:1.1}.tracks-metainfo{margin-top:1em}.tracks-metainfo__line+.tracks-metainfo__line{margin-top:2em}.tracks-metainfo__label{opacity:.5;font-weight:600}.tracks-metainfo__info{padding-top:1em;line-height:1.2}.tracks-metainfo__info>div{background-color:rgba(0,0,0,0.22);display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap}.tracks-metainfo__info>div.focus{background-color:rgba(255,255,255,0.06)}.tracks-metainfo__info>div>div{padding:1em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0}.tracks-metainfo__info>div>div:not(:last-child){padding-right:1.5em}.tracks-metainfo__info>div+div{margin-top:1em}.tracks-metainfo__column--video,.tracks-metainfo__column--name{margin-right:auto}.tracks-metainfo__column--num{min-width:3em;padding-right:0}.tracks-metainfo__column--rate{min-width:7em;text-align:right}.tracks-metainfo__column--channels{min-width:5em;text-align:right}\n    </style>\n");
     $('body').append(Lampa.Template.get('tracks_css', {}, true));
 
 })();
