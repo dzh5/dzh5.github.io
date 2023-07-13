14cfc18425066d42ce74d2049cc04752bfb00021
Requred <139059035+Requred@users.noreply.github.com>
2023-07-14 00:15:12 +0500

    love.js

--- love.js
+++ love.js
@@ -3,7 +3,7 @@
 
     var Defined = {
       use_api: 'lampac',
-      localhost: 'https://requred.github.io/plugins/love/love.json',
+      localhost: 'https://vi.sisi.am/',
       vip_site: 'http://sisi.am/vip',
       framework: ''
     };
