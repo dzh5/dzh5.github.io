40994fd9121fa06c5f5852861bdac110b2c0a630
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 02:00:50 +0500

    tsmp.js

--- tsmp.js
+++ tsmp.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    Lampa.Storage.set('torrserver_url','192.168.1.53:8090');
+    Lampa.Storage.set('torrserver_url','192.168.1.43:8090');
     Lampa.Storage.set('torrserver_url_two','localhost:8090');
     //Lampa.Storage.set('torrserver_auth','true');
     //Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || 'ts');
