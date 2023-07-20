c698ec22586595c77ce52e5409bd78a1132f531f
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 03:00:00 +0500

    tsmp.js

--- tsmp.js
+++ tsmp.js
@@ -8,6 +8,10 @@
 		Lampa.Storage.set('torrserver_url','5.42.80.114:6799');
 		Lampa.Storage.set('torrserver_url_two','torr.myftp.biz:8090');
 	}
+	if (Lampa.Platform.is('noname')) {
+		Lampa.Storage.set('torrserver_url','192.168.1.43:8090');
+		Lampa.Storage.set('torrserver_url_two','localhost:8090');
+	}
     //Lampa.Storage.set('torrserver_auth','true');
     //Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || 'ts');
     //Lampa.Storage.set('torrserver_password','ts');
