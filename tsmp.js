83d7145dc6cb1a22eddf315735242c187fcaf3f8
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 02:38:43 +0500

    tsmp.js

--- tsmp.js
+++ tsmp.js
@@ -1,10 +1,13 @@
 (function () {
-    'use strict';
- if (Lampa.Platform.is('android')) {
- 	Lampa.Storage.set('torrserver_url','192.168.1.43:8090');
-   	 Lampa.Storage.set('torrserver_url_two','localhost:8090');
-}
-    
+'use strict';
+	if (Lampa.Platform.is('android')) {
+		Lampa.Storage.set('torrserver_url','192.168.1.43:8090');
+		Lampa.Storage.set('torrserver_url_two','localhost:8090');
+	}
+    	if (Lampa.Platform.is('web')) {
+		Lampa.Storage.set('torrserver_url','5.42.80.114:6799');
+		Lampa.Storage.set('torrserver_url_two','torr.myftp.biz:8090');
+	}
     //Lampa.Storage.set('torrserver_auth','true');
     //Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || 'ts');
     //Lampa.Storage.set('torrserver_password','ts');
