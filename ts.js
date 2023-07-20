7214bbef8a3b82c9a8e4861a4d6a5a689d6e12ba
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 02:00:36 +0500

    ts.js

--- ts.js
+++ ts.js
@@ -2,7 +2,7 @@
     'use strict';
 
     Lampa.Storage.set('torrserver_url','192.168.1.72:8090');
-    Lampa.Storage.set('torrserver_url_two','192.168.1.53:8090');
+    Lampa.Storage.set('torrserver_url_two','192.168.1.43:8090');
     //Lampa.Storage.set('torrserver_auth','true');
     //Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || 'ts');
     //Lampa.Storage.set('torrserver_password','ts');
