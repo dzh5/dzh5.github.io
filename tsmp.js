88ff3036cc38066c6ed70c3bd8a951189359a422
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 02:57:39 +0500

    tsmp.js

--- tsmp.js
+++ tsmp.js
@@ -4,7 +4,7 @@
 		Lampa.Storage.set('torrserver_url','192.168.1.43:8090');
 		Lampa.Storage.set('torrserver_url_two','localhost:8090');
 	}
-    	if (Lampa.Platform.is('web')) {
+    	if (Lampa.Platform.is('browser')) {
 		Lampa.Storage.set('torrserver_url','5.42.80.114:6799');
 		Lampa.Storage.set('torrserver_url_two','torr.myftp.biz:8090');
 	}
