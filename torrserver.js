b6e0aa99c555b1758e2c2d1e97bd0ae5a722c6b5
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 21:53:40 +0500

    Update torrserver.js

--- torrserver.js
+++ torrserver.js
@@ -46,7 +46,7 @@ Lampa.SettingsApi.addParam({
      onRender: function (item) {
        setTimeout(function() {
         //$('div[data-name="torrserver_use_link"]').remove();
-        if(Lampa.Storage.field('torrserver_use_link')) item.show()&$('.settings-param__name', item).css('color','f3d900')&$('div[data-name="torrserver_use_link2"]').insertAfter('div[data-name="torrserver_url"]');
+        if(Lampa.Storage.field('torrserver_use_link')) item.show()&$('.settings-param__name', item).css('color','f3d900')&$('div[data-name="torrserver_use_link2"]').insertAfter('div[data-name="torrserver_use_link"]');
         else item.hide();
           }, 0);
         }
