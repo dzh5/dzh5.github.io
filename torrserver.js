b241b78f2daece7d4a8c842c26ada38f3906d3c4
Requred <139059035+Requred@users.noreply.github.com>
2023-07-21 22:14:47 +0500

    torrserver.js

--- torrserver.js
+++ torrserver.js
@@ -37,6 +37,7 @@ Lampa.SettingsApi.addParam({
      if (value == '8') Lampa.Storage.set('torrserver_url', '176.124.198.209:8595');
      if (value == '9') Lampa.Storage.set('torrserver_url', '5.42.82.10:8090');
      if (value == '10') Lampa.Storage.set('torrserver_url', '91.193.43.141:8090');
+     Lampa.Storage.set('torrserver_use_link', (value == '0') ? 'two' : 'one');
 	//Lampa.Storage.set('torrserver_auth','true');
 	//Lampa.Storage.set('torrserver_login',Lampa.Storage.get('account_email') || 'ts');
 	//Lampa.Storage.set('torrserver_password','ts');
