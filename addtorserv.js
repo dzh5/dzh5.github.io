53520fb97a18108cef00c7bcfe202b99466829f2
dzh5 <139059035+dzh5@users.noreply.github.com>
2023-07-30 01:01:14 +0500

    Create addtorserv.js

---
+++ addtorserv.js
@@ -0,0 +1,304 @@
+{
+    "Hash": "d7e3c6c90f52f79afbfb2f2a882db553e0f71f8e",
+    "Capacity": 10737418240,
+    "Filled": 0,
+    "PiecesLength": 4194304,
+    "PiecesCount": 30528,
+    "Torrent": {
+        "title": "Хороший доктор / The Good Doctor [S01-03] (2017-2020) WEB-DLRip 1080p | LostFilm",
+        "poster": "https://i104.fastpic.ru/big/2018/0405/25/1356acb684e7d07509d57fe471117625.jpg",
+        "data": "{\"TSA\":{\"srcUrl\":\"http://rutor.info/torrent/750664/horoshij-doktor_the-good-doctor-s01-03-2017-2020-web-dlrip-1080p-lostfilm\"}}",
+        "timestamp": 1690660802,
+        "name": "The Good Doctor [1080p] LF",
+        "hash": "d7e3c6c90f52f79afbfb2f2a882db553e0f71f8e",
+        "stat": 3,
+        "stat_string": "Torrent working",
+        "torrent_size": 128042504590,
+        "total_peers": 34,
+        "pending_peers": 34,
+        "file_stats": [
+            {
+                "id": 1,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E01.1080p.rus.LostFilm.TV.mkv",
+                "length": 1963799869
+            },
+            {
+                "id": 2,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E02.1080p.rus.LostFilm.TV.mkv",
+                "length": 1942673106
+            },
+            {
+                "id": 3,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E03.1080p.rus.LostFilm.TV.mkv",
+                "length": 1896128112
+            },
+            {
+                "id": 4,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E04.1080p.rus.LostFilm.TV.mkv",
+                "length": 1946135175
+            },
+            {
+                "id": 5,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E05.1080p.rus.LostFilm.TV.mkv",
+                "length": 1951720445
+            },
+            {
+                "id": 6,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E06.1080p.rus.LostFilm.TV.mkv",
+                "length": 1953176323
+            },
+            {
+                "id": 7,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E07.1080p.rus.LostFilm.TV.mkv",
+                "length": 1932275648
+            },
+            {
+                "id": 8,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E08.1080p.rus.LostFilm.TV.mkv",
+                "length": 1934708823
+            },
+            {
+                "id": 9,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E09.1080p.rus.LostFilm.TV.mkv",
+                "length": 1931966047
+            },
+            {
+                "id": 10,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E10.1080p.rus.LostFilm.TV.mkv",
+                "length": 1953286673
+            },
+            {
+                "id": 11,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E11.1080p.rus.LostFilm.TV.mkv",
+                "length": 1951950561
+            },
+            {
+                "id": 12,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E12.1080p.rus.LostFilm.TV.mkv",
+                "length": 1952876339
+            },
+            {
+                "id": 13,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E13.1080p.rus.LostFilm.TV.mkv",
+                "length": 1945630055
+            },
+            {
+                "id": 14,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E14.1080p.rus.LostFilm.TV.mkv",
+                "length": 1946683037
+            },
+            {
+                "id": 15,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E15.1080p.rus.LostFilm.TV.mkv",
+                "length": 1950312182
+            },
+            {
+                "id": 16,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E16.1080p.rus.LostFilm.TV.mkv",
+                "length": 1926258843
+            },
+            {
+                "id": 17,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E17.1080p.rus.LostFilm.TV.mkv",
+                "length": 1952898601
+            },
+            {
+                "id": 18,
+                "path": "The Good Doctor [1080p] LF/S01/The.Good.Doctor.S01E18.1080p.rus.LostFilm.TV.mkv",
+                "length": 1953794555
+            },
+            {
+                "id": 19,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E01.1080p.rus.LostFilm.TV.mkv",
+                "length": 2450781934
+            },
+            {
+                "id": 20,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E02.1080p.rus.LostFilm.TV.mkv",
+                "length": 2452386170
+            },
+            {
+                "id": 21,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E03.1080p.rus.LostFilm.TV.mkv",
+                "length": 2452984380
+            },
+            {
+                "id": 22,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E04.1080p.rus.LostFilm.TV.mkv",
+                "length": 2453028825
+            },
+            {
+                "id": 23,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E05.1080p.rus.LostFilm.TV.mkv",
+                "length": 2454102952
+            },
+            {
+                "id": 24,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E06.1080p.rus.LostFilm.TV.mkv",
+                "length": 2450766385
+            },
+            {
+                "id": 25,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E07.1080p.rus.LostFilm.TV.mkv",
+                "length": 2443590162
+            },
+            {
+                "id": 26,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E08.1080p.rus.LostFilm.TV.mkv",
+                "length": 2423391405
+            },
+            {
+                "id": 27,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E09.1080p.rus.LostFilm.TV.mkv",
+                "length": 2454089178
+            },
+            {
+                "id": 28,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E10.1080p.rus.LostFilm.TV.mkv",
+                "length": 2452803508
+            },
+            {
+                "id": 29,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E11.1080p.rus.LostFilm.TV.mkv",
+                "length": 2449536330
+            },
+            {
+                "id": 30,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E12.1080p.rus.LostFilm.TV.mkv",
+                "length": 2440837966
+            },
+            {
+                "id": 31,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E13.1080p.rus.LostFilm.TV.mkv",
+                "length": 2453859277
+            },
+            {
+                "id": 32,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E14.1080p.rus.LostFilm.TV.mkv",
+                "length": 2453044892
+            },
+            {
+                "id": 33,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E15.1080p.rus.LostFilm.TV.mkv",
+                "length": 2454076725
+            },
+            {
+                "id": 34,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E16.1080p.rus.LostFilm.TV.mkv",
+                "length": 2453951546
+            },
+            {
+                "id": 35,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E17.1080p.rus.LostFilm.TV.mkv",
+                "length": 2413141119
+            },
+            {
+                "id": 36,
+                "path": "The Good Doctor [1080p] LF/S02/The.Good.Doctor.S02E18.1080p.rus.LostFilm.TV.mkv",
+                "length": 2468267156
+            },
+            {
+                "id": 37,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E01.1080p.rus.LostFilm.TV.mkv",
+                "length": 2459088251
+            },
+            {
+                "id": 38,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E02.1080p.rus.LostFilm.TV.mkv",
+                "length": 2455992318
+            },
+            {
+                "id": 39,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E03.1080p.rus.LostFilm.TV.mkv",
+                "length": 2457862066
+            },
+            {
+                "id": 40,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E04.1080p.rus.LostFilm.TV.mkv",
+                "length": 2460183970
+            },
+            {
+                "id": 41,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E05.1080p.rus.LostFilm.TV.mkv",
+                "length": 2459705075
+            },
+            {
+                "id": 42,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E06.1080p.rus.LostFilm.TV.mkv",
+                "length": 2452157959
+            },
+            {
+                "id": 43,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E07.1080p.rus.LostFilm.TV.mkv",
+                "length": 2445767410
+            },
+            {
+                "id": 44,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E08.1080p.rus.LostFilm.TV.mkv",
+                "length": 2460251920
+            },
+            {
+                "id": 45,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E09.1080p.rus.LostFilm.TV.mkv",
+                "length": 2453143388
+            },
+            {
+                "id": 46,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E10.1080p.rus.LostFilm.TV.mkv",
+                "length": 2461341628
+            },
+            {
+                "id": 47,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E11.1080p.rus.LostFilm.TV.mkv",
+                "length": 2457557614
+            },
+            {
+                "id": 48,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E12.1080p.rus.LostFilm.TV.mkv",
+                "length": 2408625483
+            },
+            {
+                "id": 49,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E13.1080p.rus.LostFilm.TV.mkv",
+                "length": 2456223931
+            },
+            {
+                "id": 50,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E14.1080p.rus.LostFilm.TV.mkv",
+                "length": 2386611259
+            },
+            {
+                "id": 51,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E15.1080p.rus.LostFilm.TV.mkv",
+                "length": 2429713284
+            },
+            {
+                "id": 52,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E16.1080p.rus.LostFilm.TV.mkv",
+                "length": 2458312581
+            },
+            {
+                "id": 53,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E17.1080p.rus.LostFilm.TV.mkv",
+                "length": 2457198094
+            },
+            {
+                "id": 54,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E18.1080p.rus.LostFilm.TV.mkv",
+                "length": 2453182632
+            },
+            {
+                "id": 55,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E19.1080p.rus.LostFilm.TV.mkv",
+                "length": 2448557703
+            },
+            {
+                "id": 56,
+                "path": "The Good Doctor [1080p] LF/S03/The.Good.Doctor.S03E20.1080p.rus.LostFilm.TV.mkv",
+                "length": 2460113720
+            }
+        ]
+    },
+    "Pieces": {},
+    "Readers": []
+}
