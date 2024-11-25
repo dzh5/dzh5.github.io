74b3860f2b9b1e7f55f08d3ef61b8f8acaed4932
dzh5 <139059035+dzh5@users.noreply.github.com>
2024-11-25 20:17:42 +0500

    Update set.js

--- set.js
+++ set.js
@@ -46,8 +46,9 @@ window.lampa_settings.read_only = false;
 // Плагины
 Lampa.Storage.set('plugins', []);
 //TMDB Proxy
-//Lampa.Utils.putScriptAsync(['https://plugin.rootu.top/tmdb.js'], function () {});
-Lampa.Utils.putScriptAsync(['https://lampaplugins.github.io/store/a.js'], function () {});
+//Lampa.Utils.putScriptAsync(['https://plugin.rootu.top/tmdb.js'], function () {});	
+Lampa.Utils.putScriptAsync(['https://khuyampa.best/tmdbproxy.js'], function () {});
+//Lampa.Utils.putScriptAsync(['https://lampaplugins.github.io/store/a.js'], function () {});
 //Lampa.Utils.putScriptAsync(['https://dzh5.github.io/on/tmdb-proxy.js'], function () {});
 //Источник в меню
 Lampa.Utils.putScriptAsync(['https://dzh5.github.io/on/sources.js'], function () {});
