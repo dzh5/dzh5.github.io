0b0d8dea8a6fcaebf050353e642ef5cf9c7edd79
dzh5 <139059035+dzh5@users.noreply.github.com>
2025-02-02 01:48:56 +0500

    Update ja.js

--- on/ja.js
+++ on/ja.js
@@ -1,6 +1,7 @@
 (function() {
     'use strict';
 
+    const PLUGIN_NAME = 'jaja';
     const FAVORITE_KEY = 'favorite_jaja';
     const CORS_PROXY = 'https://cors.eu.org/';
     const SITE_CONFIG = {
@@ -365,20 +366,23 @@
 
     // Инициализация плагина
     function initPlugin() {
-        Lampa.Component.add('jaja', JajaCore);
-        addMenuEntry();
-        addStyles();
+    console.log('[Jaja] Plugin initialization started');
+    Lampa.Component.add(PLUGIN_NAME, JajaCore);
+    addMenuEntry();
+    addStyles();
+    console.log('[Jaja] Plugin initialized');
     }
+
 
     function addMenuEntry() {
         const menuItem = $(`
-            <li class="menu__item selector binded" data-action="jaja">
+            <li class="menu__item selector" data-action="${PLUGIN_NAME}">
                 <div class="menu__ico">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.18 5 4.05 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
                     </svg>
                 </div>
-                <div class="menu__text">Астра</div>
+                <div class="menu__text">18+ Контент</div>
             </li>
         `).on('hover:enter', () => showMainScreen());
 
@@ -410,8 +414,8 @@
 
     function showMainScreen() {
         Lampa.Activity.push({
-            title: 'Астра',
-            component: jaja,
+            title: '18+ Контент',
+            component: PLUGIN_NAME,
             url: SITE_CONFIG.jable.baseUrl + '/latest-updates/',
             setup: { type: 'category', category: 'Новинки' }
         });
