ad1f232adac23977c56b578dcd9c17bcbc1e0b37
dzh5 <139059035+dzh5@users.noreply.github.com>
2025-02-02 01:52:22 +0500

    Update ja.js

--- on/ja.js
+++ on/ja.js
@@ -373,21 +373,35 @@
     console.log('[Jaja] Plugin initialized');
     }
 
-
-    function addMenuEntry() {
-        const menuItem = $(`
-            <li class="menu__item selector" data-action="${PLUGIN_NAME}">
-                <div class="menu__ico">
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.18 5 4.05 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
-                    </svg>
-                </div>
-                <div class="menu__text">18+ Контент</div>
-            </li>
-        `).on('hover:enter', () => showMainScreen());
-
-        $('.menu .menu__list').first().append(menuItem);
+function addMenuEntry() {
+    console.log('[Jaja] Adding menu entry');
+    const menuHtml = `
+        <li class="menu__item selector" data-action="${PLUGIN_NAME}">
+            <div class="menu__ico">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.18 5 4.05 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>
+                </svg>
+            </div>
+            <div class="menu__text">18+ Контент</div>
+        </li>
+    `;
+    
+    const $menuList = $('.menu .menu__list:first');
+    console.log('[Jaja] Menu list element:', $menuList.length);
+    
+    if ($menuList.length) {
+        $menuList.append(menuHtml);
+        console.log('[Jaja] Menu item added successfully');
+    } else {
+        console.error('[Jaja] Menu list not found!');
+        Lampa.Listener.follow('app', e => {
+            if (e.type === 'ready') {
+                $('.menu .menu__list:first').append(menuHtml);
+                console.log('[Jaja] Menu item added after app ready');
+            }
+        });
     }
+}
 
     function addStyles() {
         const styles = `
